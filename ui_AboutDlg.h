9a4e6c3a6a4510d566b349cdfb9705488767c2b1
dominic <dominicgasperini@gmail.com>
2023-10-16 15:49:19 -0400

    created windows release and installer

--- ui_AboutDlg.h
+++ ui_AboutDlg.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'AboutDlg.ui'
 **
-** Created by: Qt User Interface Compiler version 6.3.1
+** Created by: Qt User Interface Compiler version 6.6.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -45,23 +45,23 @@ public:
     void setupUi(QDialog *AboutDlg)
     {
         if (AboutDlg->objectName().isEmpty())
-            AboutDlg->setObjectName(QString::fromUtf8("AboutDlg"));
+            AboutDlg->setObjectName("AboutDlg");
         AboutDlg->resize(280, 500);
         AboutDlg->setMinimumSize(QSize(280, 500));
         AboutDlg->setMaximumSize(QSize(280, 500));
         exitBtn = new QPushButton(AboutDlg);
-        exitBtn->setObjectName(QString::fromUtf8("exitBtn"));
+        exitBtn->setObjectName("exitBtn");
         exitBtn->setGeometry(QRect(10, 450, 261, 41));
         layoutWidget = new QWidget(AboutDlg);
-        layoutWidget->setObjectName(QString::fromUtf8("layoutWidget"));
+        layoutWidget->setObjectName("layoutWidget");
         layoutWidget->setGeometry(QRect(10, 10, 261, 431));
         verticalLayout_3 = new QVBoxLayout(layoutWidget);
-        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setObjectName("verticalLayout_3");
         verticalLayout_3->setContentsMargins(0, 0, 0, 0);
         verticalLayout = new QVBoxLayout();
-        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setObjectName("verticalLayout");
         TitleBtn = new QPushButton(layoutWidget);
-        TitleBtn->setObjectName(QString::fromUtf8("TitleBtn"));
+        TitleBtn->setObjectName("TitleBtn");
         TitleBtn->setStyleSheet(QString::fromUtf8("font: 500 oblique 25pt \"DejaVu Sans\";"));
 
         verticalLayout->addWidget(TitleBtn);
@@ -71,15 +71,15 @@ public:
         verticalLayout->addItem(verticalSpacer);
 
         gridLayout = new QGridLayout();
-        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        gridLayout->setObjectName("gridLayout");
         authorLbl = new QLabel(layoutWidget);
-        authorLbl->setObjectName(QString::fromUtf8("authorLbl"));
+        authorLbl->setObjectName("authorLbl");
         authorLbl->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
 
         gridLayout->addWidget(authorLbl, 0, 0, 1, 1);
 
         authorNameLbl = new QLabel(layoutWidget);
-        authorNameLbl->setObjectName(QString::fromUtf8("authorNameLbl"));
+        authorNameLbl->setObjectName("authorNameLbl");
         authorNameLbl->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
 
         gridLayout->addWidget(authorNameLbl, 0, 1, 1, 1);
@@ -91,35 +91,35 @@ public:
         verticalLayout_3->addLayout(verticalLayout);
 
         LogoLbl = new QLabel(layoutWidget);
-        LogoLbl->setObjectName(QString::fromUtf8("LogoLbl"));
+        LogoLbl->setObjectName("LogoLbl");
         LogoLbl->setAlignment(Qt::AlignCenter);
 
         verticalLayout_3->addWidget(LogoLbl);
 
         verticalLayout_2 = new QVBoxLayout();
-        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setObjectName("verticalLayout_2");
         gridLayout_2 = new QGridLayout();
-        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        gridLayout_2->setObjectName("gridLayout_2");
         qtVersionNumLbl = new QLabel(layoutWidget);
-        qtVersionNumLbl->setObjectName(QString::fromUtf8("qtVersionNumLbl"));
+        qtVersionNumLbl->setObjectName("qtVersionNumLbl");
         qtVersionNumLbl->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
 
         gridLayout_2->addWidget(qtVersionNumLbl, 1, 1, 1, 1);
 
         qtVersionLbl = new QLabel(layoutWidget);
-        qtVersionLbl->setObjectName(QString::fromUtf8("qtVersionLbl"));
+        qtVersionLbl->setObjectName("qtVersionLbl");
         qtVersionLbl->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
 
         gridLayout_2->addWidget(qtVersionLbl, 1, 0, 1, 1);
 
         cppVersionLbl = new QLabel(layoutWidget);
-        cppVersionLbl->setObjectName(QString::fromUtf8("cppVersionLbl"));
+        cppVersionLbl->setObjectName("cppVersionLbl");
         cppVersionLbl->setAlignment(Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter);
 
         gridLayout_2->addWidget(cppVersionLbl, 0, 0, 1, 1);
 
         cppVersionNumLbl = new QLabel(layoutWidget);
-        cppVersionNumLbl->setObjectName(QString::fromUtf8("cppVersionNumLbl"));
+        cppVersionNumLbl->setObjectName("cppVersionNumLbl");
         cppVersionNumLbl->setAlignment(Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter);
 
         gridLayout_2->addWidget(cppVersionNumLbl, 0, 1, 1, 1);
@@ -128,7 +128,7 @@ public:
         verticalLayout_2->addLayout(gridLayout_2);
 
         qtLogoLbl = new QLabel(layoutWidget);
-        qtLogoLbl->setObjectName(QString::fromUtf8("qtLogoLbl"));
+        qtLogoLbl->setObjectName("qtLogoLbl");
         qtLogoLbl->setAlignment(Qt::AlignCenter);
 
         verticalLayout_2->addWidget(qtLogoLbl);
